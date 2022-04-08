c3258d585fc9f3510059c855750da67493adc8b2
KhoaPham-dev <namkhoapham@gmail.com>
2022-04-09 02:26:26 +0700

    Fix domain name

--- public/login/index.js
+++ public/login/index.js
@@ -1,4 +1,4 @@
-const listMemberUrl = "https://tamnhinxuyentheky.harrisstudio.org/list-member/";
+const listMemberUrl = "https://anhsangthoidai.harrisstudio.org/list-member/";
 let loginButton = document.getElementById("login-button");
 firebase.auth().onAuthStateChanged(function(user) {
     setupUI(user);
